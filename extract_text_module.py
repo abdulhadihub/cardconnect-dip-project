a6120e25b8601425d65c2f97e874b9f047aae7c4
Abdul Hadi <abdulhadi.reality@gmail.com>
2023-11-12 21:30:49 +0500

    better contour detection

--- extract_text_module.py
+++ extract_text_module.py
@@ -13,28 +13,20 @@ class ImageProcessor:
         input_image = cv2.imread(input_image_path)
 
         if deskew:
-            # Assuming ImageTransformer is defined in a separate file
             from deskew_image_module import ImageTransformer
-
-            # Create an instance of ImageTransformer
             image_transformer = ImageTransformer(input_image)
-
-            # Transform the image
             transformed_image = image_transformer.transform_image()
-
-            # Use the transformed image for processing
             return self._process_image_internal(transformed_image, showROI)
         else:
-            # Process the image without deskewing
             return self._process_image_internal(input_image, showROI)
 
     def _process_image_internal(self, image, showROI):
         base_image = image.copy()
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (7, 7), 0)
+        blur = cv2.GaussianBlur(gray, (11, 11), 0)
         thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (50, 20))
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (50, 5))
         dilate = cv2.dilate(thresh, kernel, iterations=1)
 
         contours = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
